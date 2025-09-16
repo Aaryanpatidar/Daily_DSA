22c1b4b2598a02a8900ac9a0773258dbd3d29e04
Aaryanpatidar <aaryanpatidar1211@gmail.com>
2025-09-16 15:45:12 +0530

    16/09 Q-2

---
+++ NeetCode150/ValidAnagram/Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public static boolean isAnagram(String s1, String s2) {
+       int [] arr = new int[26];
+       if(s1.length() != s2.length())return false;
+       for(int i =0; i<s1.length(); i++) {
+            int x = s1.charAt(i) - 'a';
+            int y = s2.charAt(i) - 'a';
+            arr[x]++;arr[y]--;
+       }
+       for(int i =0; i<26; i++) {
+            if(arr[i] != 0)return false;
+       }
+       return true;
+    }
+
+    public static void main(String[] args) {
+        String s1 = "anagram", s2 = "nagaram";
+        boolean ans = isAnagram(s1,s2);
+    }
+}
