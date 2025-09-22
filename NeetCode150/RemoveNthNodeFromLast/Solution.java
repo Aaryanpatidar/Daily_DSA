b57800be842c25a4b62332fbe55002efff8d9184
Aaryanpatidar <aaryanpatidar1211@gmail.com>
2025-09-22 14:33:27 +0530

    22.09 Q-5

---
+++ NeetCode150/RemoveNthNodeFromLast/Solution.java
@@ -0,0 +1,27 @@
+class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+ 
+class Solution {
+    
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        
+        // 2 Pointer Approach
+        // jab right null pohochega to left uss node pr khada hoga jisko nikalna h
+        ListNode l = new ListNode(0,head), r = head, dummy = l;
+        while(n>0){
+            r=r.next;
+            n--;
+        }
+        while(r!=null){
+            l=l.next;
+            r=r.next;
+        }
+        l.next=l.next.next;
+        return dummy.next;
+    }
+}
