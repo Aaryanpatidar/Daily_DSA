f2146ccea8d7916c79cd0a39e856b17cf243364f
Aaryanpatidar <aaryanpatidar1211@gmail.com>
2025-09-23 16:00:13 +0530

    23/09 Q-6 revision

---
+++ NeetCode150/DiameterOfBinaryTree/Solution.java
@@ -0,0 +1,29 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+ 
+class Solution {
+    
+    public static int fun(TreeNode root, int ans[]){
+        if(root == null)return 0;
+        int left = fun(root.left,ans);
+        int right = fun(root.right,ans);
+        ans[0]=Math.max(ans[0],(left+right));
+        return Math.max(left,right)+1;
+    }
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        int []ans = new int[1];
+        fun(root,ans);
+        return ans[0];
+    }
+}
