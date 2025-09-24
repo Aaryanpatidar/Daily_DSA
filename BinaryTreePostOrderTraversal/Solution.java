c9cc704596bb61c4bbc6e83ad8ff2945ce619769
Aaryanpatidar <aaryanpatidar1211@gmail.com>
2025-09-24 11:10:37 +0530

    24.09 BT-4

---
+++ BinaryTreePostOrderTraversal/Solution.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+ 
+class Solution {
+    public static void rec(TreeNode root,List<Integer>ans){
+        if(root==null)return;
+        rec(root.left,ans);
+        rec(root.right,ans);
+        ans.add(root.val);
+    }
+
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer>ans = new ArrayList<>();
+        rec(root,ans);
+        return ans;
+    }
+}
